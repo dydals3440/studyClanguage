bec83d01bf752da1e832107ad773b57490df777e
dydals3440 <dydals3440@gmail.com>
2023-01-09 13:57:07 +0900

    덧셈 뺼셈 공부 c언어

---
+++ add_sub_integer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    int num2;
+
+    num1 = 1 + 2;
+    num2 = 1 - 2;
+
+    printf("%d\n", num1);
+    printf("%d\n", num2);
+
+    return 0;
+}
+/*
+3
+-1
+*/
